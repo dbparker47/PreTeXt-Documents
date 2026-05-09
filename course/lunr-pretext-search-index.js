0e6769fb80acd262f4569016e6943b9aff072ae2
dbparker47 <dbparker47@gmail.com>
2026-05-09 17:21:35 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Syllabus for Math 325, Section 01",
+  "body": " Course Syllabus for Math 325, Section 01        General Course Information     Course Title  Discrete Structures: Computer Science 2    Class Meets:  MWF 12:00-12:50pm (MAK A-2-167)    Blackboard Page Link   https:\/\/lms.gvsu.edu\/        Drop-In Office Hours  If you find yourself having trouble with any part of this course, please come to my drop-in office hours for help. You should also feel welcome to set up appointments with me outside of office hours, including in the evening on Zoom if necessary. My schedule is on Blackboard in the Course Information & Resources document.   Monday  2:00-3:00 (PCS & Zoom)    Tuesday  11:30-12:30 (Zoom only)    Wednesday  10:00-11:00 (Office & Zoom)    Thursday  11:30-12:30 (Office & Zoom)    Friday  11:00-11:50 (Office & Zoom)   Note that all Monday office hours will be held in the Peer Collaboration Space (PCS), which is on the second floor of Mackinac Hall, Room A-2-610, across from the Math Center. Also, here is the Zoom link for office hours .    Instructor Information     Name  Darren Parker    Office  MAK A-2-150    E-Mail   parkerda@gvsu.edu        Course Content   Course Description  Properties of relations, equivalence relations, partial orderings, fundamental concepts of graphs, trees, digraphs, networks, and associated algorithms; computer science applications.    Prerequisite  Math 225    Course Objectives     Compute basic information about graphs, relations, and trees.    Solve theoretical and applied problems involving applications of basic concepts of graphs, relations, and trees.    Formulate computational problems in terms of graphs, relations, and trees.    Construct a logical framework for a proof using mathematical induction, direct proof, proof by contraposition, or proof by induction.    Analyze the structure and validity of a mathematical proof.    Employ effective problem-solving skills in solving computational problems.    Explain methods and solutions of computational problems in a clear way to a specified target audience.    Demonstrate fluency in applying algorithms in the formulation and solutions of mathematical problems.    Assess one's own work in mathematical problem solving and apply feedback to make improvements to one's own work.        Course Resources   Textbooks  There was not one textbook that was suitable for every topic of the course, so we will be using three different textbooks for different topics. Do not panic, though; each textbook is available on Blackboard free as a pdf file, and two of them are available in interactive HTML form. Here are the textbooks, along with links to the textbook website and\/or the HTML version.    Mathematical Reasoning: Writing and Proof, by Ted Sundstrom : Here is the textbook website      Applied Combinatorics, by Mitchel Keller and William Trotter : Here are the textbook website and HTML version of the textbook .     Applied Discrete Structures, by Al Doerr and Ken Levasseur : Here are the textbook website and HTML version of the textbook .       Zoom  I will have in-person drop-in office hours on Monday, Wednesday, Thursday (mostly), and Friday. However, my office hours on Tuesdays (and sometimes Thursdays) will be on Zoom only. In addition, you have the option of meeting me on Zoom during my in-person office hours. You can install Zoom from the link https:\/\/gvsu-edu.zoom.us\/ .    PDF Scanner App  While you are welcome to turn in your assignments in hard copy form, all assignments may also be turned in as PDF files on Blackboard. Any handwritten work must be scanned before it is submitted electronically. Some examples of PDF scanner apps are Google Drive, Abobe Scan, and Notebloc. Each of these can be downloaded from Google Play or the App Store for free.    Adobe Acrobat Reader  I will be grading all electronically submitted assignments using Adobe Acrobat Reader. To read my comments on your assignments, you need to install Adobe Acrobat Reader on your computer and\/or phone. You can download it for free at https:\/\/get.adobe.com\/reader\/       Assessments and Grades   Grading  You will be graded on coding assignments, homework, quizzes, three midterm exams, and a final exam.   Coding Assignments  10%    Weekly Homework  10%    Quizzes  35%    Exams  38%    Final Exam  7%   Grades cutoffs will generally be as follows: 92% for A; 90% for A-; 88% for B+; 82% for B; 80% for B-; 78% for C+; 72% for C; 70% for C-; 68% for D+; and 60% for D.    Coding Assignments  One of the objectives of this course is for you to learn about connections between mathematics and computer science. This includes using what you learn in mathematics to inform your coding, but it also includes using coding to address problems in mathematics. You will be assigned weekly coding assignments using the programming language Python. You can use Codecademy or Learn Python if you need to learn or review the language. All programs must be clearly and thoroughly documented. Coding assignments are posted in the Homework & Coding Assignments course module of Blackboard and will be due on Sundays. You will have the opportunity to resubmit each coding assignment once.    Homework Assignments  Each week, I will assign some problems for homework. For each problem, you must explain your reasoning in complete sentences as well as computations. Homework assignments will be posted in the Homework & Coding Assignments course module of Blackboardand will be due on Sundays. You will have the opportunity to resubmit each homework assignment once.    Quizzes  For each section of the textbook, there is a quiz (sometimes more) for the content covered. Once we complete a section of the textbook, I will bring copies of that quiz to class for you to take. Here are the ground rules on quizzes.   You may choose which day to take each quiz. It may either be taken after announcements during any class or in person at my office during office hours or an office appointment.    Each problem on a quiz comes directly from the activities we work on in class.    Quizzes generally take around 10 minutes if you are prepared.    You may not use any notes or technology on quizzes except a non-networked calculator (not your phone or computer).    You will have the opportunity to re-take quizzes as many times as you want. I prepare two versions of each quiz, so if you re-take the quiz, you might have a different version than the first time you take it. For each quiz, your final score is the highest score of all attempts. All versions of quizzes contain only problems from the in-class activities.       Exams  There will be three exams and a final exam. All problems come from your homework assignments (except for the final exam). No notes or technology are allowed during any exams except a non-networked calculator (not your phone or computer). Each exam (except the final exam) will be given during two class periods. In the first class period, you make as much progress as you can, and then you turn in the exam to me. Five days later, I will return your exam to you in class so that you can finish the exam. Between the first and second parts of the exam, the exam problems will be available to you so that you can study them and ask questions.   Exam 1  Friday 9\/26 and Wednesday 10\/1    Exam 2  Friday 10\/24 and Wednesday 10\/29    Exam 3  Wednesday 11\/19 and Monday 11\/24       Final Exam  The final exam is Wednesday, December 10 at 8:00-9:50 am. The format will be a bit different than the other exams. On Monday the week before the exam, I will make available a few problems from the content not covered on the other exams. These will be the problems that will be on the final exam. On exam day, you will have the full exam time to work on these problems. If you finish early, you may do one or more of the following.   You may work on any problems from previous exams. If you choose to do this, you must give me the graded version of your previous exam before you begin work on the exam problems.    You may re-take any quiz.       Class Format  Our class meets three times each week. You will need to spend a significant amount of time preparing for each class. All preparation materials are found on the Class Preparation Resources and Course Links course module on Blackboard. There you will find a folder for every week of class, and each weekly folder has a document of course resources for each class meeting. There are also links to homework assignments, coding assignments, quiz problems, and information & resources for exams that week. The document for each class meeting has the following.   Links to videos explaining the content of the section. You are expected to watch these for each class.    Along with each video, a link to a set of activities that you should work on before class. These are the problems that will be on the quizzes.    An optional reading from the textbook, in case you want to consult another source.   Here is what a typical class period will be like.   We will spend a short time on announcements and answering questions about that day's work.    You will then have an opportunity to take a quiz on any content previously covered in class. You may choose any quiz, which could mean re-taking a quiz you have already taken. See explanation of quizzes above for more information.    The bulk of class time will be for working in groups on the in-class activities. As noted above, all quiz problems come from these activities.    If there is time at the end, we may work on some of the problems from the in-class activities as a class.       Course Policies      Absences: You are expected to attend class regularly unless you are ill or have some other emergency. You are responsible for being aware of any material covered and announcements made in class. The FigJam boards that have our notes from class will be available to you, as well as any videos and class activities. I will be happy to help you on Zoom. Also, you may make an office appointment with me to make up any missed work.     Remote Classes: In the event that we are unable to meet in class (e.g. if I am ill or if there is a weather emergency), we will meet as a class on Zoom. The link for the class is available on Blackboard, and I will send out the link in an email before any online class session begins.     Late Assignments: If you are unable to turn in an assignment on time, I will generally allow you to turn in your assignment a little late. You should give me as much notice as you can and make arrangements with me to complete your assignment as early as possible. Note that this will likely cause your assignment to be graded later than those for the rest of the class.     University Course Policies: For information on policies that apply to all GVSU courses, please go to https:\/\/www.gvsu.edu\/coursepolicies .     Academic Integrity: Everyone is expected to practice academic honesty at all times. This includes the following.   No student shall knowingly, without authorization, procure, provide, or accept any materials that contain questions or answers to any submitted work in the class.    No student shall, without authorization, complete, in part or in total, any submitted work for another person.    No student shall, without authorization, allow any submitted work to be completed, in part or in total, by another person.    No student shall knowingly plagiarize or copy the work of another person and submit it as their own. This includes sentences, paragraphs or entire articles copied from books, periodicals, speeches, or the writing of other students. It also includes artificial intelligence tools.    No student shall submit work that has been previously graded.   Violations of these rules may result in failure of the assignment or failure of the course.     Use of Generative Artificial Intelligence: Generative AI tools may be used in limited circumstances in this course. I give some guidance below, but the main takeaway is that you may use AI to help you learn and understand the concepts we cover in class, but you may not use AI to do the thinking and explanations necessary to complete your work on assignments. In particular, you may not turn in an assignment in which you cannot explain the reasoning in your problem solutions. Acceptable uses include:   Generating problems of a certain type to practice problem-solving techniques.    Generating examples to better understand a concept.    Showing you the syntax for something you need for your code in Python (e.g. syntax for creating a dictionary).    Identifying errors in your computations or logic (but not correcting them).   Unacceptable uses include:   Generating a solution to a problem on an assignment.    Writing explanations for your computations or proofs on an assignment.    Generating code for a coding assignment.    Documenting your code in a coding assignment.   Violations of these rules may result in failure of the assignment or failure of the course.       Other Information   Important Dates:    January 11: Classes begin    January 15: 100% tuition refund deadline; last day to add a class (5:00 pm)    January 18: Martin Luther King Jr. Day (no classes)    February 5: 75% tuition refund deadline; last day for CR\/NC changes (5:00 pm)    March 6-14: Spring Break (no classes)    March 26: Deadline for withdrawing from a class (5:00 pm)    April 23: Classes end    April 24 - May 1: Exam Week    May 6: Grades available to students    Important Information:    If you are in need of accommodations due to a learning, physical, or other disability you must have Student Accessibility Resources (SAR) send me a memo indicating the existence of a disability and the suggested reasonable accommodations. If you have not already done so, please contact the Student Accessibility Resources office (215 The Blue Connection) by calling 616-331-2490 or email to access@gvsu.edu . Please note that I cannot provide accommodations based upon disability until I have received a SAR issued memo. All discussions will remain confidential.    If you have a physical disability and think you will need assistance evacuating this classroom and\/or building in an emergency situation, please make me aware so I can develop a plan to assist you.   I reserve the right to adjust this syllabus as needed and will notify you of any changes through Blackboard.    "
+},
+{
+  "id": "office-hours-2",
+  "level": "2",
+  "url": "syllabus.html#office-hours-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Course Information & Resources "
+},
+{
+  "id": "textbooks-2",
+  "level": "2",
+  "url": "syllabus.html#textbooks-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Mathematical Reasoning: Writing and Proof, by Ted Sundstrom Applied Combinatorics, by Mitchel Keller and William Trotter Applied Discrete Structures, by Al Doerr and Ken Levasseur "
+},
+{
+  "id": "coding-assignments-2",
+  "level": "2",
+  "url": "syllabus.html#coding-assignments-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Homework & Coding Assignments "
+},
+{
+  "id": "homework-assignments-2",
+  "level": "2",
+  "url": "syllabus.html#homework-assignments-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Homework & Coding Assignments "
+},
+{
+  "id": "class-format-2",
+  "level": "2",
+  "url": "syllabus.html#class-format-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Class Preparation Resources and Course Links "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-01-intro-activity",
+  "level": "1",
+  "url": "activity-01-intro-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Introduction to Proofs",
+  "body": " Introduction to Proofs    Solve the following problems. Write some kind of outline your proof (which can be a ``Know-Show'' table, but does not have to be).      Let be an odd integer. Determine whether is odd or even. Prove your result.      Let and be odd integers. Determine whether is odd or even. Prove your result.    "
+},
+{
+  "id": "activity-01-intro-activity-3",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Let be an odd integer. Determine whether is odd or even. Prove your result.   "
+},
+{
+  "id": "activity-01-intro-activity-4",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Let and be odd integers. Determine whether is odd or even. Prove your result.   "
+},
+{
+  "id": "handouts",
+  "level": "1",
+  "url": "handouts.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Handouts",
+  "body": " Handouts    "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
